cca25a658e15e23b6026134490c9de52fde399ae
undefined <nodeexpressreact@gmail.com>
2020-08-11 10:15:32 +0500

    first

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-   uri: "mongodb://mongo-node-api-prod:27017/node-api",
+  uri: "mongodb://mongo-node-api-prod:27017/node-api",
   // uri: "mongodb://localhost:27017/node-api",
+  //uri: "mongodb://64.227.64.36:27020"
 };
