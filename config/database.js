106cafca43e7bccebcc54754a1404fa1c2835f3b
KHDS <danyal_saleem@hotmail.com>
2022-07-11 12:02:49 +0000

    with db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-  uri: "mongodb://64.227.24.36:27020/node-api"
-  // uri: "mongodb://localhost:27017/node-api",
-  //uri: "mongodb://64.227.64.36:27020"
+  uri: "mongodb://64.227.24.36:27027/node-api"
 };
